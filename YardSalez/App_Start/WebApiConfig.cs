87e244626f4dac841e768fd4731d2059b0b7fa78
Sai Gudigundla <saip106@msn.com>
2014-08-28 23:48:35 -0400

    added items controller

--- YardSalez/App_Start/WebApiConfig.cs
+++ YardSalez/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace YardSalez
 {
@@ -9,14 +6,11 @@ namespace YardSalez
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                name: "Default",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ YardSalez/Items/ItemsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace YardSalez.Items
+{
+    public class ItemsController : ApiController
+    {
+        private readonly IList<Item> _items = new List<Item>(); 
+
+        public IEnumerable<Item> Get()
+        {
+            return _items;
+        }
+
+        public Item Get(int id)
+        {
+            return _items.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Post([FromBody]Item item)
+        {
+            _items.Add(item);
+        }
+
+        public void Put(int id, [FromBody]string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
